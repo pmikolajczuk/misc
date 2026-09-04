bb33a69ef9a97bc46ed52501103b0fdea9f2d59d
pmikolajczuk <pidus@o2.pl>
2026-09-04 20:39:27 +0200

    Telusko Spring tutorial - initial commit

---
+++ Java/tutorials/telusko/spring/springdemoapp/src/main/java/pl/mikolaj/springdemoapp/SpringdemoappApplication.java
@@ -0,0 +1,13 @@
+package pl.mikolaj.springdemoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdemoappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringdemoappApplication.class, args);
+	}
+
+}
