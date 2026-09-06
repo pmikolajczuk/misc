bec647165e5224abc3c6d94d77dc5fa0ea1371db
pmikolajczuk <pidus@o2.pl>
2026-09-06 16:22:20 +0200

    Telusko Spring Simple Web App tutorial - initial commit

---
+++ Java/tutorials/telusko/spring/simplewebapp/src/test/java/pl/mikolaj/simplewebapp/SimplewebappApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.mikolaj.simplewebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplewebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
