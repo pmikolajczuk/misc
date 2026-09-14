5d632b42674202de99bd73b83f2dd56dcbd20622
pmikolajczuk <pidus@o2.pl>
2026-09-14 14:17:44 +0200

    Telusko Spring Security Tutorial - initial commit

---
+++ Java/tutorials/telusko/spring/springsecex/src/main/java/pl/mikolaj/springsecex/SpringsecexApplication.java
@@ -0,0 +1,13 @@
+package pl.mikolaj.springsecex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsecexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringsecexApplication.class, args);
+	}
+
+}
---
+++ Java/tutorials/telusko/spring/springsecex/src/test/java/pl/mikolaj/springsecex/SpringsecexApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.mikolaj.springsecex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
